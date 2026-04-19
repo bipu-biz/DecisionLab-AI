b66713fe345c8864ad31bcd766a3d98cac3b167d
bipu-biz <bipulbharti2006@gmail.com>
2026-04-20 01:49:57 +0530

    setup backend and mongodb connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=>{
+    try{
+        const connection = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`mongodb connected host ${connection.connection.host}`)
+    }
+
+    catch(error){
+        console.log('mongodb failed to connect',error.message)
+        process.exit(1)
+    }
+}
+
+export default connectDB
