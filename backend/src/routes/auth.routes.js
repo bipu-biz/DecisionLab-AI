b9c79cd3bb8bebdbbedc0969d7d5a9da65e5b96a
bipu-biz <bipulbharti2006@gmail.com>
2026-04-27 19:09:47 +0530

    fix register issue

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ import { signup,login} from '../controllers/auth.controller.js'
 
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/register", signup);
 router.post("/login", login);
 
 export default router;
